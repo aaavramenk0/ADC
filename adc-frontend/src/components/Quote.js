f973b7bc3f64e61890d78d10bef4aabe4d7208c7
Aleksandr <alex.avramenko2004@gmail.com>
2023-02-22 20:54:28 -0500

    Quote adaptation

--- adc-frontend/src/components/Quote.js
+++ adc-frontend/src/components/Quote.js
@@ -4,7 +4,7 @@ const Quote = () => {
     return (
         <section className="quote">
             <p className="quote__text">
-                «A website is a powerfull communication tool, a great way to <br/> build trust and credibility and an essential part of any marketing strategy»
+                «A website is a powerfull communication tool, a great way to build trust and credibility and an essential part of any marketing strategy»
             </p>
             <p className="quote__author">
                 – Brian Tracy, Canadian-American motivational public speaker
