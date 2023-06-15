56bdcd64d4192202c6b744e5523055f0788b4494
Aleksandr <alex.avramenko2004@gmail.com>
2023-06-15 18:09:24 -0400

    Finishing Login and Signup functions for the user in the backend

--- adc-backend/controllers/userController.js
+++ adc-backend/controllers/userController.js
@@ -1,8 +1,26 @@
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken'
+
+// create JSON web token function
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+}
+
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.login(email, password);
+
+        // create a JWT
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token })
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 
@@ -13,7 +31,10 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password);
 
-        res.status(200).json({ email, user });
+        // create a JWT
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token })
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- adc-backend/server.js
+++ adc-backend/server.js
@@ -1,9 +1,17 @@
+/* 
+    - Import of the necessary modules and libraries for the application, such as Express, Mongoose, and dotenv.
+    - An instance of the Express application is created and stored in the app variable.
+    - The PORT constant is set to the value of the environment variable PORT or 8000 as a fallback.
+    - The dotenv module is used to load environment variables from a .env file.
+*/
 import express from "express";
-import { initDb, getDb } from "./db/connect.js"; // When you are using ECMAScript modules you are forced to provide the file extension = > NOT ../db/connect BUT ../db/connect.js
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import { router as userRoutes } from './routes/user.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+dotenv.config();
 
 // middleware
 app.use(express.json());
@@ -14,14 +22,24 @@ app.use((req, res, next) => {
 })
 
 
-// routes 
+
+/* 
+    This line sets up the route /api/user and associates it with the userRoutes router, which is imported from the ./routes/user.js file. This allows the application to handle user-related API endpoints.
+*/ 
 app.use('/api/user', userRoutes);
 
-initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(PORT);
-        console.log(`Connected to DB and listening on ${PORT}`);
-    }
-})
+/* 
+    - The code establishes a connection to the MongoDB database specified by the MONGODB_URI environment variable using Mongoose.
+    - If the connection is successful, the application starts listening for incoming requests on the specified PORT, and a success message is logged to the console.
+    - If an error occurs during the database connection, the error is caught and logged to the console.
+*/
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log('connected to db & listening on port', PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
